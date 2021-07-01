00d74148da9974a2f39d457313696ec935fd7175
y00njaekim <rladbswo12@gist.ac.kr>
2021-07-01 11:33:44 +0900

    try

--- app/src/main/java/com/example/madcamp1_2/MainActivity.java
+++ app/src/main/java/com/example/madcamp1_2/MainActivity.java
@@ -1,35 +1,53 @@
-package com.example.myapplication;
+package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        /// ... 코드 계속
 
-        // ... 코드 계속
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs) ;
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int pos = tab.getPosition() ;
+                changeView(pos) ;
+            }
 
-        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
-        tabHost1.setup() ;
+            /// ... 코드 생략
+        }) ;
+    }
 
-        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
-        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
-        ts1.setContent(R.id.content1) ;
-        ts1.setIndicator("TAB 1") ;
-        tabHost1.addTab(ts1)  ;
+    private void changeView(int index) {
+        TextView textView1 = (TextView) findViewById(R.id.text1) ;
+        TextView textView2 = (TextView) findViewById(R.id.text2) ;
+        TextView textView3 = (TextView) findViewById(R.id.text3) ;
 
-        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
-        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
-        ts2.setContent(R.id.content2) ;
-        ts2.setIndicator("TAB 2") ;
-        tabHost1.addTab(ts2) ;
+        switch (index) {
+            case 0 :
+                textView1.setVisibility(View.VISIBLE) ;
+                textView2.setVisibility(View.INVISIBLE) ;
+                textView3.setVisibility(View.INVISIBLE) ;
+                break ;
+            case 1 :
+                textView1.setVisibility(View.INVISIBLE) ;
+                textView2.setVisibility(View.VISIBLE) ;
+                textView3.setVisibility(View.INVISIBLE) ;
+                break ;
+            case 2 :
+                textView1.setVisibility(View.INVISIBLE) ;
+                textView2.setVisibility(View.INVISIBLE) ;
+                textView3.setVisibility(View.VISIBLE) ;
+                break ;
 
-        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
-        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3") ;
-        ts3.setContent(R.id.content3) ;
-        ts3.setIndicator("TAB 3") ;
-        tabHost1.addTab(ts3) ;
+        }
     }
 }
